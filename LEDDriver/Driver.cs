970994d1d2026ef4ee6604ac88da66b13b4f9677
JeremyCBrooks <brooksjeremyc@gmail.com>
2016-02-02 13:16:35 -0500

    initial

---
+++ LEDDriver/Driver.cs
@@ -0,0 +1,93 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace LEDDriver
+{
+    public enum LED
+    {
+        Red = 1,
+        Green = 2,
+        Blue = 3
+    }
+
+    public class Driver
+    {
+        private static readonly string fifo = "blink.fifo";
+        private static readonly string driver = "blink.py";
+
+        private Process driverProcess;
+
+        public void Start()
+        {
+            if(driverProcess != null)
+            {
+                //already running, call Stop() first
+                return;
+            }
+
+            //look for already running process
+            var processes = Process.GetProcesses();
+            foreach (var process in processes)
+            {
+                if (process.ProcessName == driver)
+                {
+                    driverProcess = process;
+                    break;
+                }
+            }
+
+            if (driverProcess == null)
+            {
+                driverProcess = new Process();
+                driverProcess.StartInfo = new ProcessStartInfo(driver)
+                {
+                    UseShellExecute = false,
+                };
+                driverProcess.Exited += DriverProcess_Exited;
+                driverProcess.Start();
+            }
+        }
+
+        private void DriverProcess_Exited(object sender, System.EventArgs e)
+        {
+            driverProcess = null;
+        }
+
+        public bool IsRunning
+        {
+            get { return driverProcess != null; }
+        }
+
+        public bool Stop()
+        {
+            return writeValue("x");
+        }
+
+        public bool EnableLED(LED led)
+        {
+            return writeValue(led.ToString());
+        }
+
+        public bool DisableAllLEDs()
+        {
+            return writeValue("0");
+        }
+
+        private bool writeValue(string value)
+        {
+            if(driverProcess == null)
+            {
+                //driver not running
+                return false;
+            }
+
+            using (var fs = File.OpenWrite(fifo))
+            using (var sr = new StreamWriter(fs))
+            {
+                sr.Write(value);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,39 @@
+﻿
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var driver = new LEDDriver.Driver();
+
+            driver.Start();
+            System.Console.Out.WriteLine("Start");
+            System.Threading.Thread.Sleep(1000);
+            System.Console.Out.WriteLine($"Is running: {driver.IsRunning}");
+
+            driver.EnableLED(LEDDriver.LED.Red);
+            System.Console.Out.WriteLine("Set red");
+            System.Threading.Thread.Sleep(1000);
+
+            driver.EnableLED(LEDDriver.LED.Green);
+            System.Console.Out.WriteLine("Set green");
+            System.Threading.Thread.Sleep(1000);
+
+            driver.EnableLED(LEDDriver.LED.Blue);
+            System.Console.Out.WriteLine("Set blue");
+            System.Threading.Thread.Sleep(1000);
+
+            driver.DisableAllLEDs();
+            System.Console.Out.WriteLine("All off");
+            System.Threading.Thread.Sleep(1000);
+
+            driver.Stop();
+            System.Console.Out.WriteLine("Stop");
+            System.Threading.Thread.Sleep(1000);
+
+            System.Console.Out.WriteLine($"Is running: {driver.IsRunning}");
+            System.Threading.Thread.Sleep(1000);
+        }
+    }
+}
